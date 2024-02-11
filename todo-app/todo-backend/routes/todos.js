51df42b97622f04a502a27e6aa90204ad9ccf4b3
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2024-02-11 18:57:22 +0100

    12.7: Add get and put methods.

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,14 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+ Object.assign(req.todo, req.body);
+ await req.todo.save();
+ res.send(req.todo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
